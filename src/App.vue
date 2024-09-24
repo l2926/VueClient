904ce102ace1bdd6a077c1e9ca71a3521590d24c
李子健 <zjli1003@126.com>
2024-09-24 22:04:48 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeNavigation></HomeNavigation>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeNavigation from './components/HomeNavigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeNavigation
   }
 }
 </script>
---
+++ src/components/HomeNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navigation">
+    <p>
+      <el-link href="#/" >首页</el-link>
+      <el-link href="#/industry/industry_list?level=L1" >板块</el-link>
+      <el-link href="#/finance/industry_list?level=L1">财务</el-link>
+      <el-link href="#/statistics/industry_list?level=L1">统计</el-link>
+      <el-link target="_blank" @click="onSelectAnalysis">分析</el-link>
+      <el-link to="#/test" target="_blank">对比</el-link>
+      <el-link href="#/test" target="_blank">国际</el-link>
+      <el-link href="#/test" target="_blank">衍生品</el-link>
+
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+import {useRouter} from "vue-router";
+import {getCurDate} from "@/Api/utils/calcDate";
+
+const router = useRouter()
+const onSelectAnalysis = () =>{
+  const query_dic = {
+    "id":"1",
+    "trade_date":getCurDate()
+  };
+  router.push({path:"/analysis/limit",query:query_dic});
+}
+
+</script>
+
+
+<style scoped>
+.navigation{
+  text-align:right;
+}
+a {
+  color: #FF4040;
+  font-weight: bold;
+  width:45px;
+}
+</style>
