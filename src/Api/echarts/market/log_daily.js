2aba4fe02da68bead642543aa7d131441b8e8718
李子健 <zjli1003@126.com>
2025-06-28 13:22:03 +0800

    【新增】Log日线图

--- src/Api/echarts/market/log_daily.js
+++ src/Api/echarts/market/log_daily.js
@@ -12,8 +12,13 @@ export function InitLogDailyECharts(rawData){
     });
 
     var data = priceData.map(item =>{
+        return [+item["open"], +item["close"], +item["low"], +item["high"]];
+    });
+
+    var log_data = priceData.map(item =>{
         return [+item["log_open"], +item["log_close"], +item["log_low"], +item["log_high"]];
     });
+
     //
     // var amount = priceData.map(item => {
     //     return item["amount"];
@@ -23,22 +28,22 @@ export function InitLogDailyECharts(rawData){
         return item['total_mv']
     });
 
-    var pe = priceData.map(item=>{
-        return item["pe"]
-    })
+    // var pe = priceData.map(item=>{
+    //     return item["pe"]
+    // })
+    //
+    // var pb = priceData.map(item=>{
+    //     return item["pb"]
+    // })
 
-    var pb = priceData.map(item=>{
-        return item["pb"]
-    })
-
-    var roe = priceData.map(item=>{
-        if(item['pe'] == 0){
-            item['roe'] = 0;
-        }else{
-            item['roe'] = item['pb'] / item['pe'];
-        }
-        return item['roe'].toFixed(2);
-    })
+    // var roe = priceData.map(item=>{
+    //     if(item['pe'] == 0){
+    //         item['roe'] = 0;
+    //     }else{
+    //         item['roe'] = item['pb'] / item['pe'];
+    //     }
+    //     return item['roe'].toFixed(2);
+    // })
 
     var pct_chg = priceData.map(item=>{
         return item["pct_chg"]
@@ -52,13 +57,13 @@ export function InitLogDailyECharts(rawData){
         return item["turn_over_rate_f"]
     })
 
-    var asset = priceData.map((item=>{
-        if(item["pb"] != 0 && item["pb"] != null){
-            return (item["total_mv"] / item["pb"]).toFixed(2)
-        }else{
-            return 0
-        }
-    }))
+    // var asset = priceData.map((item=>{
+    //     if(item["pb"] != 0 && item["pb"] != null){
+    //         return (item["total_mv"] / item["pb"]).toFixed(2)
+    //     }else{
+    //         return 0
+    //     }
+    // }))
 
     const option = {
         title: {
@@ -158,49 +163,17 @@ export function InitLogDailyECharts(rawData){
                 yAxisIndex:0,
                 data: data,
             },{
-                name: '成交金额',
-                type: 'bar',
+                name: '对数行情',
+                type: 'candlestick',
                 xAxisIndex:1,
                 yAxisIndex:1,
-                data:pct_chg
+                data:log_data
             },{
                 name: '市值',
                 type: 'line',
                 xAxisIndex:2,
                 yAxisIndex:2,
                 data:total_mv,
-                itemStyle:{
-                    normal:{
-                        color:'transparent'
-                    }
-                }
-            },{
-                name: '市盈率',
-                type: 'line',
-                xAxisIndex:1,
-                yAxisIndex:3,
-                symbol:"none",
-                data:pe,
-            },{
-                name: '市净率',
-                type: 'line',
-                xAxisIndex:1,
-                yAxisIndex:4,
-                symbol:"none",
-                data:pb,
-            },{
-                name: 'ROE',
-                type: 'line',
-                xAxisIndex:2,
-                yAxisIndex:5,
-                symbol:"none",
-                data:roe,
-            },{
-                name: '涨跌幅',
-                type: 'line',
-                xAxisIndex:0,
-                yAxisIndex:6,
-                data:pct_chg,
                 itemStyle:{
                     normal:{
                         color:'transparent'
@@ -229,11 +202,18 @@ export function InitLogDailyECharts(rawData){
                     }
                 }
             },{
-                name: '净资产',
+                name: '涨跌幅',
                 type: 'bar',
                 xAxisIndex:2,
                 yAxisIndex:8,
-                data:asset
+                data:pct_chg,
+                itemStyle: {
+                    // 每个柱子设置颜色
+                    color: function(params) {
+                        const value = params.value;
+                        return value >= 0 ? 'green' : 'red';
+                    }
+                }
             }
         ],
     };
