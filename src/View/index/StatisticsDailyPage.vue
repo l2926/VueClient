093f2d17643d06f0c5c6eb86924d8ff5dc2b3294
李子健 <zjli1003@126.com>
2025-07-17 17:29:17 +0800

    【修复】统计模块，上方的标题头页面

--- src/View/index/StatisticsDailyPage.vue
+++ src/View/index/StatisticsDailyPage.vue
@@ -2,6 +2,12 @@
   <el-header>
     <StatisticsNavigation></StatisticsNavigation>
   </el-header>
+
+  <div style="display: flex; justify-content: center;">
+    <div id="upDiv">大盘上涨个数:,</div>
+    <div id="downDiv">大盘下跌个数:</div>
+  </div>
+
   <div id="priceCharts" class="container-fluid" style="width:99%;height:600px;margin-left: 0"></div>
 
 </template>
@@ -18,10 +24,23 @@ const route = useRoute();
 const axiosEcharts = ()=>{
   // alert("statistics");
   const query_dic = JSON.parse(JSON.stringify(route.query));
+
+  //标题头数据
+  axios.post("http://127.0.0.1:8081/index/statistics_count",query_dic).then(
+      response=>{
+        // alert(JSON.stringify(response.data));
+        document.getElementById("upDiv").innerText = "大盘上涨个数:" + response.data.up_count + ",";
+        document.getElementById("downDiv").innerText = "大盘下跌个数:" + response.data.down_count;
+      }
+  ).catch(error=>{
+    alert(error)
+  })
+
+  //正文图表
   // alert(JSON.stringify(query_dic))
   axios.post("http://127.0.0.1:8081/index/statistics",query_dic).then(
       response=>{
-        alert(JSON.stringify(response.data));
+        // alert(JSON.stringify(response.data));
         try{
           InitStatisticsECharts(response.data);
         }
