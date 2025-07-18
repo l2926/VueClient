92d50e00ad4ce8bf15ad0985f7f1fa05c482ff26
李子健 <zjli1003@126.com>
2025-07-18 17:45:48 +0800

    【开发】统计模块，上方总览开发

--- src/View/index/StatisticsMvPage.vue
+++ src/View/index/StatisticsMvPage.vue
@@ -8,6 +8,14 @@
     <div id="upDiv" style="color:red"></div>
     <div id="downText">,大盘下跌个数:</div>
     <div id="downDiv" style="color: green"></div>
+    <div id="shText">,上证指数:</div>
+    <div id="shPct"></div>
+    <div id="szText">,深证指数:</div>
+    <div id="szPct"></div>
+    <div id="smallText">,中小板指数:</div>
+    <div id="smallPct"></div>
+    <div id="startupText">,创业板指数:</div>
+    <div id="startupPct"></div>
   </div>
 
   <div id="priceCharts" class="container-fluid" style="width:99%;height:600px;margin-left: 0"></div>
@@ -33,6 +41,37 @@ const axiosEcharts = ()=>{
         // alert(JSON.stringify(response.data));
         document.getElementById("upDiv").innerText = response.data.up_count;
         document.getElementById("downDiv").innerText = response.data.down_count;
+        document.getElementById("shPct").innerText = response.data.sh_pct;
+        document.getElementById("szPct").innerText = response.data.sz_pct;
+        document.getElementById("smallPct").innerText = response.data.small_pct;
+        document.getElementById("startupPct").innerText = response.data.startup_pct;
+
+        if(response.data.sh_pct > 0){
+          document.getElementById("shPct").style.color='red';
+        }
+        if(response.data.sz_pct > 0){
+          document.getElementById("szPct").style.color='red';
+        }
+        if(response.data.small_pct > 0){
+          document.getElementById("smallPct").style.color='red';
+        }
+        if(response.data.startup_pct > 0){
+          document.getElementById("startupPct").style.color='red';
+        }
+
+        if(response.data.sh_pct < 0){
+          document.getElementById("shPct").style.color='green';
+        }
+        if(response.data.sz_pct < 0){
+          document.getElementById("szPct").style.color='green';
+        }
+        if(response.data.small_pct < 0){
+          document.getElementById("smallPct").style.color='green';
+        }
+        if(response.data.startup_pct < 0){
+          document.getElementById("startupPct").style.color='green';
+        }
+
       }
   ).catch(error=>{
     alert(error)
