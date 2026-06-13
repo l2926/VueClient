fd34c8d7f564b2eb0e10e2cf199c691283119d24
李子健 <zjli1003@126.com>
2026-06-13 16:54:19 +0800

    【开发】涨停历史行情

--- src/View/analysis/WeekMarketOverview.vue
+++ src/View/analysis/WeekMarketOverview.vue
@@ -259,7 +259,7 @@ const axiosTable = ()=>{
   // alert(JSON.stringify(para_dic))
   axios.post("http://127.0.0.1:8081/analysis/week_overview",para_dic).then(
       (response) => {
-        alert(JSON.stringify(response.data));
+        // alert(JSON.stringify(response.data));
         tableData.value = response.data;
 
         var ts_code_list = response.data.map((item)=>{
